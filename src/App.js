98b83ab223bdfa110676d672cb6e720edd22d3d9
Victor Heringer <victorhugo.heringer@hotmail.com>
2019-01-01 20:04:34 -0200

    @change some aspects of default files that come with create react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+
+import Home from './containers/Home';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar/>
+        <Router>
+          <div>
+            <Route path="/" exact component={Home} />
+          </div>
+        </Router>
+      </React.Fragment>
     );
   }
 }

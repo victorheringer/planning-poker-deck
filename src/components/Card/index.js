9ce79733bef547abd8e3421e4064f3f7ee0412fc
Victor Heringer <victorhugo.heringer@hotmail.com>
2019-01-02 19:41:42 -0200

    @add some functions

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CardFactory from './../../factories/CardFactory';
 import './index.css';
 import './patterns.css';
 
@@ -7,10 +9,14 @@ import './patterns.css';
  * 
  * Renders a card component
  * 
- * @props {Number} number 
+ * @props {Number} value 
  * @props {Bool} fliped
  * @props {Bool} static
  * @props {String} pattern
+ * @props {Bool} editing
+ * @props {Bool} icon
+ * @props {Function} onClickRemove
+ * @props {Function} onClick
  */
 class Card extends Component {
 
@@ -23,25 +29,37 @@ class Card extends Component {
    */
   handleClick = event => {
     event.preventDefault();
-    !this.props.static && event.currentTarget.classList.toggle('flipped');
+    if (this.props.static) {
+      const card = CardFactory.create(this.props.value, this.props.icon);
+      this.props.onClick(event, card);
+    }
+    else {
+      !this.props.static && event.currentTarget.classList.toggle('flipped');
+    }
+  }
+
+  handleClickRemove = event => {
+    this.props.onClickRemove(event, this.props.value);
   }
 
   render() {
 
-    const { number, up } = this.props;
+    const { value, up, editing, icon } = this.props;
     const flipped = up ? ' flipped' : '';
 
     return (
       <div className="cardWrapper">
         <div className="cardContainer">
           <div className="close">
-            <button>X</button>
+            {editing && <button onClick={this.handleClickRemove}>X</button>}
           </div>
           <div className={'card' + flipped} onClick={this.handleClick}>
             <div className="front">
             </div>
             <div className="back tech-pattern">
-              <h3 className="cardTitle cardNumber">{number}</h3>
+              <h3 className="cardTitle cardNumber">
+                {icon ? <FontAwesomeIcon icon={value} /> : value}
+              </h3>
             </div>
           </div>
         </div>

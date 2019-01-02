07b59896559cbd78dacb22eeb95c05ca4313a6aa
Victor Heringer <victorhugo.heringer@hotmail.com>
2019-01-02 19:39:20 -0200

    @import font awesome and add icons to lib

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faMugHot } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faEdit);
+library.add(faMugHot);
 
 ReactDOM.render(
   <App/>, 

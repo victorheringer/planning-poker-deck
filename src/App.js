f5a09ffdec042da5366c0e17ff24699f6361764c
Victor Heringer <victorhugo.heringer@hotmail.com>
2019-01-04 20:38:37 -0200

    @add route for rules container

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-import Home from './containers/Home';
 import Navbar from './components/Navbar';
+import Home from './containers/Home';
+import Rules from './containers/Rules';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faEdit,
   faMugHot,
-  faLongArrowAltLeft
+  faLongArrowAltLeft,
+  faInfoCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faEdit);
 library.add(faMugHot);
 library.add(faLongArrowAltLeft);
+library.add(faInfoCircle);
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar/>
         <Router>
           <div>
+            <Navbar />
             <Route path="/" exact component={Home} />
+            <Route path="/rules" exact component={Rules} />
           </div>
         </Router>
       </React.Fragment>

fce34847eb32be21b6ed8eb8392fab88f7d73f82
Victor Heringer <victorhugo.heringer@hotmail.com>
2019-01-04 20:37:43 -0200

    @add some links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './index.css';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import navIcon from './../../assets/img/menu.svg';
 import githubIcon from './../../assets/img/github.svg';
 import deckIcon from './../../assets/img/deck.svg';
+import './index.css';
 
 /**
  * @author Victor Heringer
@@ -33,12 +35,17 @@ class Navbar extends Component {
 
     const links = [
       {
+        name: "Como Jogar",
+        url: "https://github.com/VictorHeringer/planning-poker-deck",
+        icon: githubIcon
+      },
+      {
         name: "Github",
         url: "https://github.com/VictorHeringer/planning-poker-deck",
         icon: githubIcon
       }
     ];
-    const deckIcon = <img className="navIcon" src={deckIcon} alt="menu" />;
+    const d = <img className="navIcon" src={deckIcon} alt="menu" />;
     return (
       <React.Fragment>
         <div className="navbar">
@@ -58,22 +65,36 @@ class Navbar extends Component {
                     </a>
                     <ul>
                       <div className="navContainer">
-                      {
-                        links.map(link => {
-                          return (
-                            <li key={link.name}>
-                              <a href={link.url} target="_blank" rel="noopener noreferrer">
-                                <img 
-                                  className="icon" 
-                                  src={link.icon} 
-                                  alt="icon" 
-                                />
-                                {link.name}
-                              </a>
-                            </li>
-                          );
-                        })
-                      }
+                        <li onClick={this.handleClick}>
+                          <Link to="/">
+                            <img
+                              className="icon"
+                              src={deckIcon}
+                              alt="icon"
+                            />
+                            Jogar
+                          </Link>
+                        </li>
+                        <li onClick={this.handleClick}>
+                          <Link to="/rules">
+                            <FontAwesomeIcon icon={"info-circle"} />
+                            &nbsp; Como Jogar
+                          </Link>
+                        </li>
+                        <li>
+                          <a 
+                            href="https://github.com/VictorHeringer/planning-poker-deck" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                          >
+                            <img 
+                              className="icon" 
+                              src={githubIcon} 
+                              alt="icon" 
+                            />
+                            Github
+                          </a>
+                        </li>
                       </div>
                     </ul>
                   </li>

1f95b40229a1986c4640bd07e13d9d2a64f780a1
Victor Heringer <victorhugo.heringer@hotmail.com>
2021-09-25 19:19:10 -0300

    WIP

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,18 @@
+import React, { Suspense, lazy } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import { Preferences, Offline, Lobby, Online, Decks } from "./screens";
+import { Offline } from "./screens";
 import { Page, Navbar, Container, ConnectionBanner } from "components";
 import { Screens } from "enums";
+
+const Preferences = lazy(() => import("./screens/preferences"));
+const Lobby = lazy(() => import("./screens/lobby"));
+const Online = lazy(() => import("./screens/online"));
+const Decks = lazy(() => import("./screens/decks"));
 
 type RoutesProps = {
   theme: Theme;
@@ -21,26 +27,18 @@ export default function Routes({ theme, initial, text }: RoutesProps) {
         <Navbar theme={theme} />
         <Page>
           <ConnectionBanner text={text.offline} />
-          <Switch>
-            <Route exact path="/">
-              <Redirect to={`/${initial}`} />
-            </Route>
-            <Route exact path={`/${Screens.HOME}`}>
-              <Offline />
-            </Route>
-            <Route exact path={`/${Screens.DECKS}`}>
-              <Decks />
-            </Route>
-            <Route path={`/${Screens.SETTINGS}`}>
-              <Preferences />
-            </Route>
-            <Route path={`/${Screens.LOBBY}`}>
-              <Lobby />
-            </Route>
-            <Route path={`/${Screens.ONLINE}`}>
-              <Online />
-            </Route>
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to={`/${initial}`} />
+              </Route>
+              <Route exact path={`/${Screens.HOME}`} component={Offline} />
+              <Route exact path={`/${Screens.DECKS}`} component={Decks} />
+              <Route path={`/${Screens.SETTINGS}`} component={Preferences} />
+              <Route path={`/${Screens.LOBBY}`} component={Lobby} />
+              <Route path={`/${Screens.ONLINE}`} component={Online} />
+            </Switch>
+          </Suspense>
         </Page>
       </Router>
     </Container>
